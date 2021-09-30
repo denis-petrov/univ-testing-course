bb13d4ceb159444a5154b03145fb62f16a94d74f
Denis Petrov <den11032001@mail.ru>
2021-09-30 23:40:29 +0300

    Updated 2 lab

--- labs/lab2/src/main/java/com/app/App.java
+++ labs/lab2/src/main/java/com/app/App.java
@@ -1,5 +1,7 @@
 package com.app;
 
+import java.util.HashSet;
+
 public class App {
 
     private static final String PATH_TO_INPUT = "./data/input.txt";
@@ -9,7 +11,7 @@ public class App {
     public static void main(String[] args) {
         try {
             var urls = FileProcessor.parseUrlsFromInput(PATH_TO_INPUT);
-            var result = UrlProcessor.crawlingThroughUrls(urls, true);
+            var result = UrlProcessor.crawlingThroughUrls(urls);
             var brokenLinks = result.getValue0();
             var stableLinks = result.getValue1();
             FileProcessor.writeMapToFile(brokenLinks, PATH_TO_INVALID_LINKS);
--- labs/lab2/src/main/java/com/app/UrlProcessor.java
+++ labs/lab2/src/main/java/com/app/UrlProcessor.java
@@ -9,24 +9,24 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class UrlProcessor {
 
     private static final Pattern URL_PATTERN = Pattern.compile("^(https?|ftp|file)://([-a-zA-Z0-9+&@#/%?=~_|!:,.;][^/]*)[-a-zA-Z0-9+&@#/%=~_|]");
+    private static final Pattern EXCEPTION_PATTERN = Pattern.compile(".*(Status=(\\d*)),.*");
+    private static final Set<String> VISITED_PAGES = new HashSet();
 
     private UrlProcessor() {
     }
 
     public static Pair<Map<String, String>, Map<String, String>> crawlingThroughUrls(
-            List<String> urls,
-            boolean isNeedToProcessNestedLinks
+            List<String> urls
     ) {
         var brokenLinks = new LinkedHashMap<String, String>();
         var stableLinks = new LinkedHashMap<String, String>();
@@ -38,10 +38,12 @@ public final class UrlProcessor {
                         .execute();
 
                 if (response.statusCode() != HttpURLConnection.HTTP_OK)
-                    throw new RuntimeException();
+                    throw new RuntimeException(response.statusMessage() + ":" + response.statusCode());
 
-                if (isNeedToProcessNestedLinks)
+                if (!VISITED_PAGES.contains(url)) {
+                    VISITED_PAGES.add(url);
                     processNestedLinks(brokenLinks, stableLinks, url);
+                }
 
                 stableLinks.putIfAbsent(url, "stable");
             } catch (Exception e) {
@@ -51,23 +53,39 @@ public final class UrlProcessor {
         return new Pair<>(brokenLinks, stableLinks);
     }
 
+    private static void processException(LinkedHashMap<String, String> brokenLinks, String url, Exception e) {
+        Matcher matcher = EXCEPTION_PATTERN.matcher(e.toString());
+        String message = e.getMessage() + (matcher.find() ? ":" + matcher.group(2) : "");
+        brokenLinks.putIfAbsent(url, message);
+    }
+
     private static void processNestedLinks(
             Map<String, String> brokenLinks,
             Map<String, String> stableLinks,
             String url
     ) throws IOException {
+        Predicate<String> predicate = URL_PATTERN.asPredicate();
+        String urlWithoutPage = isLinkPage(url) ? getCleanUrl(url) : url;
         List<String> linksFromPage = findLinksFromPage(url).stream()
-                .filter(URL_PATTERN.asPredicate())
+                .filter(UrlProcessor::isLinkPage)
+                .map(link -> predicate.test(link) ? link : urlWithoutPage + link)
+                .filter(s -> !VISITED_PAGES.contains(s))
                 .collect(Collectors.toList());
-        var nestedLinks = crawlingThroughUrls(linksFromPage, false);
+        var nestedLinks = crawlingThroughUrls(linksFromPage);
         brokenLinks.putAll(nestedLinks.getValue0());
         stableLinks.putAll(nestedLinks.getValue1());
     }
 
-    private static void processException(LinkedHashMap<String, String> brokenLinks, String url, Exception e) {
-        Matcher matcher = URL_PATTERN.matcher(url);
-        String message = isReceivedMessageNonCorrect(e, matcher) ? "link does not exist, received 404 code" : e.getMessage();
-        brokenLinks.putIfAbsent(url, message);
+    private static boolean isLinkPage(String url) {
+        return url.contains(".html") || url.contains(".php");
+    }
+
+    private static String getCleanUrl(String url) {
+        List<Integer> indexes = IntStream
+                .iterate(url.indexOf("/"), index -> index >= 0, index -> url.indexOf("/", index + 1))
+                .boxed()
+                .collect(Collectors.toList());
+        return url.substring(0, indexes.get(indexes.size() - 1)) + "/";
     }
 
     private static Set<String> findLinksFromPage(String url) throws IOException {
@@ -81,9 +99,5 @@ public final class UrlProcessor {
         return elements.stream()
                 .map(element -> element.attr("href"))
                 .collect(Collectors.toSet());
-    }
-
-    private static boolean isReceivedMessageNonCorrect(Exception e, Matcher matcher) {
-        return matcher.find() && matcher.group(2).equals(e.getMessage());
     }
 }
