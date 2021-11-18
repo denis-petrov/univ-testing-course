6b58b51c91062f077c36c6b8bb4c755c3f560f21
Denis Petrov <den11032001@mail.ru>
2021-11-18 11:25:26 +0300

    Code after review

--- labs/lab8/src/derivative-currencies-service.js
+++ labs/lab8/src/derivative-currencies-service.js
@@ -2,22 +2,21 @@ import mbHelper from "./mountebank-helper.js"
 import settings from "./settings.js"
 
 const addService = () => {
-    const response = {
+    const USD = {
         "USD-RUB": {
             "date": "Nov 13, 2021, 13:13 UTC",
             "value": "72.8717"
         },
-        "EUR-RUB": {
-            "date": "Nov 13, 2021, 13:14 UTC",
-            "value": "83.3884"
-        },
-        "CHF-RUB": {
-            "date": "Nov 13, 2021, 13:14 UTC",
-            "value": "79.1076"
-        },
         "USD-CHF": {
             "date": "Nov 13, 2021, 13:17 UTC",
             "value": "0.921172"
+        }
+    }
+
+    const EUR = {
+        "EUR-RUB": {
+            "date": "Nov 13, 2021, 13:14 UTC",
+            "value": "83.3884"
         },
         "EUR-CHF": {
             "date": "Nov 13, 2021, 13:17 UTC",
@@ -25,27 +24,106 @@ const addService = () => {
         }
     }
 
+    const CHF = {
+        "CHF-RUB": {
+            "date": "Nov 13, 2021, 13:14 UTC",
+            "value": "79.1076"
+        }
+    }
+    const allCurrencies = {
+        USD, EUR, CHF
+    }
+
     const stubs = [
         {
-            predicates: [{
-                equals: {
-                    method: "GET",
-                    "path": "/"
+            predicates: [
+                {
+                    equals: {
+                        method: "GET",
+                        "path": "/",
+                    }
                 }
-            }],
+            ],
             responses: [
                 {
                     is: {
                         statusCode: 200,
                         headers: {
-                            "Content-Type": "application/json"
+                            "Content-Type": "application/json",
+                            "Location": "http://localhost:5001/",
                         },
-                        body: JSON.stringify(response)
+                        body: JSON.stringify(allCurrencies)
+                    },
+                },
+            ]
+        },
+        {
+            predicates: [
+                {
+                    equals: {
+                        method: "GET",
+                        "path": "/USD",
                     }
                 }
+            ],
+            responses: [
+                {
+                    is: {
+                        statusCode: 200,
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Location": "http://localhost:5001/USD",
+                        },
+                        body: JSON.stringify(USD)
+                    },
+                },
+            ]
+        },
+        {
+            predicates: [
+                {
+                    equals: {
+                        method: "GET",
+                        "path": "/EUR",
+                    }
+                }
+            ],
+            responses: [
+                {
+                    is: {
+                        statusCode: 200,
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Location": "http://localhost:5001/EUR",
+                        },
+                        body: JSON.stringify(EUR)
+                    },
+                },
+            ]
+        },
+        {
+            predicates: [
+                {
+                    equals: {
+                        method: "GET",
+                        "path": "/CHF",
+                    }
+                }
+            ],
+            responses: [
+                {
+                    is: {
+                        statusCode: 200,
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Location": "http://localhost:5001/CHF",
+                        },
+                        body: JSON.stringify(CHF)
+                    },
+                },
             ]
         }
-    ];
+    ]
 
     const imposter = {
         port: settings.derivative_currencies_service_port,
